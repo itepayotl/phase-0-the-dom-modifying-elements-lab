c0c4f965dcf9050d9645fae2fe9d966525d21770
Israel <itepayotl@yahoo.com>
2023-08-01 17:27:19 -0700

    complete job

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const elementToRemove = document.getElementById("main");
+elementToRemove.remove();
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+const yourName = 'John'; 
+newHeader.textContent = `${yourName} is the champion`;
